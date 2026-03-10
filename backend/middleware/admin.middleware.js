544fc69955e5b88d9be1cee5972c0158fb4fdf53
Muirama <mandresy.michel.ramandimbinirina@esti.mg>
2026-03-10 09:13:30 +0300

    Middleware update

--- backend/middleware/admin.middleware.js
+++ backend/middleware/admin.middleware.js
@@ -1,6 +1,4 @@
 const adminMiddleware = (req, res, next) => {
-  // Ce middleware s'utilise TOUJOURS après authMiddleware
-  // car il a besoin de req.user
   if (!req.user) {
     return res.status(401).json({ message: "Accès refusé. Non authentifié." });
   }
